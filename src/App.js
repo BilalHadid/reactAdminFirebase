9ee703091c596f584393a6631434972f894858db
bilalhadid <hadid0648@gmail.com>
2020-11-29 18:16:11 +0500

    AMMM

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Admin } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+//connect the data provider to the REST endpoint
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+function App() {
+  return <Admin dataProvider={dataProvider} />;
+}
+
+export default App;
